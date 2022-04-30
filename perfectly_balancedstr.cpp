26662ebe8801c5d2e6b8396fe13d72b7982b7b2d
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-04-30 21:19:11 +0530

    update

---
+++ perfectly_balancedstr.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#define pn puts("No");
+#define py puts("Yes");
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string k;
+        cin>>k;
+        
+    }
