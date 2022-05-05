fcafac38d16985d3268fd0d81ae58b289d0a9208
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-05 13:46:57 +0530

    update

---
+++ 786div3F.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <math.h>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    test{
+        int n,m,k;
+        
+    }
+    return 0;}
