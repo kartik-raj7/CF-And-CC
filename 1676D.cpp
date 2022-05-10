64b2ab3f2706ee989ea9f28405e6d7d0dcdf1957
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-11 00:19:45 +0530

    update

---
+++ 1676D.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <math.h>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<=(h);i++)
+using namespace std;
+int main(){
+test{
+int n,m;
+cin>>n>>m;
+int arr[n][m];
+loop(i,0,n){
+    loop(j,0,m){
+        cin>>arr[i][j];
+    }
+}
+}
+return 0;}
