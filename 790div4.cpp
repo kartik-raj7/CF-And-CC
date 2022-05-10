e944bee2e1822d53f36b460bc988a97e0291de81
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-10 22:54:08 +0530

    update

---
+++ 790div4.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <math.h>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<=(h);i++)
+using namespace std;
+int main(){
+test{
+string n;
+cin>>n;
+int l=0,r=0;
+for(int i=0;i<3;i++){
+    l+=(n[i]-'0');
+}
+for(int i=5;i>=3;i--){
+    r+=(n[i]-'0');
+}
+if(l==r){py;}
+else pn;}
+    return 0;
+    }
