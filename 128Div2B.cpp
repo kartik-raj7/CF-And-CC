efeeb09f92752d76b23241e3d035e9a6119efd1f
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-14 12:46:50 +0530

    update

---
+++ 128Div2B.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <math.h>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+map<string,string>mp;
+test{
+int k,n;
+cin>>k>>n;
+char c[k][n];int a=INT_MAX,b=INT_MAX;
+for(int i=0;i<k;i++){
+    for(int j=0;j<n;j++){
+        cin>>c[i][j];
+        if(c[i][j]=='R'){
+            a=min(a,i);
+            b=min(b,j);
+        }
+    }
+}
+if(c[a][b]=='R'){
+    py;
+}
+else pn;
+}
+}
+
+
