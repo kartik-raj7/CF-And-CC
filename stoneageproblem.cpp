12ee9a8f7a609f8e78220d7baf6f39e91d0bdf4f
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-15 19:34:32 +0530

    update

--- stoneageproblem.cpp
+++ stoneageproblem.cpp
@@ -22,44 +22,69 @@
 #define test int t; cin>>t; while(t--)
 #define ll long long
 int v[999999];
+int a[200010];
+int b[200010];
 #define loop(i,l,h) for(ll i=(l);i<(h);i++)
 using namespace std;
-int main(){
-ll n,q,t,m,x,sum,p=0;
-map<long,long> mm;
-	cin>>n>>q;
-	for(int i=1;i<=n;i++)
+signed main(){
+{  
+    int n,m,sum = 0,c = 0,x = 0;
+    cin >> n >> m;
+	for(int i=1; i<=n; i++) cin >> a[i];
+	for(int i=1; i<=n; i++) sum += a[i];
+	while(m--)
 	{
-		long long tmp;
-		cin>>tmp;
-		sum+=tmp;
-		mm[i]=tmp;
-	}
-	for(int i=0;i<q;i++)
-	{
-		cin>>t;
-		if(t==1)
+		int s,s1,s2;
+		cin >> s >> s1;
+		if(s == 1)
 		{
-			cin>>m>>x;
-			if(mm[m])
-			{
-				sum+=(x-mm[m]);
-				mm[m]=x;
-			}
+			cin >> s2;
+			if(b[s1] == c) sum += (s2-a[s1]);
 			else
 			{
-				sum+=(x-p);
-				mm[m]=x;
+			    b[s1] = c;
+			    sum += (s2-x);
 			}
-			cout<<sum<<endl;
+			cout << sum << endl;
+			a[s1] = s2;
 		}
-		else if(t==2)
+		else
 		{
-			cin>>x;
-			mm.clear();
-			p=x;
-			sum=x*n;
-			cout<<sum<<endl;
+			c++;
+			x = s1;
+			sum = s1 * n;
+			cout << sum << endl;
 		}
 	}
+	return 0;
 }
+}
+// ll n,q,t,m,x,sum,p=0;
+// ll arr[10000001];
+// 	cin>>n>>q;
+// 	for(int i=1;i<=n;i++)
+// 	{
+// 		cin>>arr[i];
+// 		sum+=arr[i];
+// 	}
+// 	for(int i=0;i<q;i++)
+// 	{
+// 		cin>>t>>m;
+// 		if(t==1){
+// 			cin>>x;
+// 			if(arr[m]){
+// 				sum+=(x-arr[m]);
+// 			}
+// 			else{
+// 				sum+=(x-p);
+// 			}
+// 			cout<<sum<<endl;
+// 			arr[m]=x;
+// 		}
+// 		else{
+// 			p = m;
+// 			sum = m*x;
+// 			cout<<sum<<endl;
+// 		}
+// 	}
+// }
