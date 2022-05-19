145ba3c8e5ba21bf5dde21db1fb4aa291358b938
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-20 00:25:05 +0530

    update

---
+++ codeforcesb.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    test{
+       ll a,b,c;
+       cin>>a>>b>>c;
+       cout<<a+b+c<<" "<<b+c<<" "<<c<<endl;
+    }
+}
