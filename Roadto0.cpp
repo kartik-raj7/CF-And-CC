01b19c310a144dcab3e573f5cc9a9840b40d7d7e
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-19 18:56:19 +0530

    update

---
+++ Roadto0.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <math.h>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+test{
+ll x,y,a,b,ans;
+		cin>>x>>y;
+		cin>>a>>b;
+		if (x>y) swap(x,y);
+		ans=(y-x)*a;
+		ans+=min(x*b,(2*x)*a);
+		cout<<ans<<endl;
+	}
+}
