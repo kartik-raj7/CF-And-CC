89266f169f4bf6f4493dace3acb58ad1d4495f74
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-05-21 23:40:09 +0530

    update

---
+++ aaa.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test int t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<"I hate";
+    for(int i=1;i<n;i++){
+        (i&1)?cout<<" that I love":cout<<" that I hate";
+        }
+        cout<<" it";
+    }
