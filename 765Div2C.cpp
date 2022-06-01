0531c0ce66b69470b0e7f5bdd831fb6d7ab5b55a
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-06-01 21:34:27 +0530

    update

---
+++ 765Div2C.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+   test{
+      ll n,q;
+      cin>>n>>q;
+      string str;
+      cin>>str;
+      
+      
+
+   }
+}
