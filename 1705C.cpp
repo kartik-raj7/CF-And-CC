5d02317a701048848afc2cefca99f90c0fa7a5f7
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-07-23 16:56:21 +0530

    update

---
+++ 1705C.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+int v[999999];
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    test{
+        // int size,q,n;
+        // cin>>size>>q>>n;
+        // multimap<int,char>mp;
+        // string str;
+        // cin>>str;
+        // for(int i=0;i<str.size();i++){
+        //     char a = str[i];
+        //     int k = i+1;
+        //     mp.insert({k,a});
+        // }
+        // while(q--){
+        //     int a;int b ;
+        //     cin>>a>>b;
+        //     a--;
+        //     for(int i=a;i<=b-a;i++){
+        //         mp.insert({i+1,str[i]});
+        //     }
+        //     // string c=str.substr(a,b-a);
+        //     // str+=c;
+            
+        // }
+        // while(n--){
+        //     int sp;
+        //     cin>>sp;
+        //     char c = *mp.find(sp);
+        //     cout<<c<<endl;
+        //     // cout<<str[sp-1]<<endl;
+        // }
+        
+    }
+}
