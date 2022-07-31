7dce67d20161563de08d81422e45529e517f7a38
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-07-31 22:19:33 +0530

    update

---
+++ CodetonB.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    test{
+    int n,x;
+    cin>>n>>x;
+    
+    }
