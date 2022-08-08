f73a1cae556b8eceb88f54b629cb8eb09ccd8fac
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-08 21:29:52 +0530

    update

---
+++ 1713C.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int main(){
+    
+}
