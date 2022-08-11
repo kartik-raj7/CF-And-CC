907b9e1a41c73e245155563491fb9d2692908277
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-11 22:45:23 +0530

    update

---
+++ 519C.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+#include <set>
+#include <limits>
+#define pb push_back
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <string.h>
+#include <string>
+#define MOD 1000000007
+#define pn puts("NO");
+#define py puts("YES");
+#define test ll t; cin>>t; while(t--)
+#define ll long long
+#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+using namespace std;
+int solve(int a,int b){
+    if(a==0||b==0)return 0;
+    if(a+b<3)return 0;
+    else return 1+solve(a-2,b-1);
+}
+int main(){
+    int xp;
+    int nb;
+    cin>>xp>>nb;
+    int counter=0;
+    if(xp==nb){
+        counter = (xp+nb)/3;
+    }
+    // while(xp>=1&&nb>=1){
+        else if(xp>=nb&&xp>=2&&nb>=1){
+           counter=solve(xp,nb);
+        }
+        else if(xp<=nb&&nb>=2&&xp>=1){
+           counter+=solve(nb,xp);
+        }
+        // else{
+        //     break;
+        // }
+   cout<<counter<<endl;
+}
