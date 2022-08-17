1c8f88dd9cd0c592e342ce45eea889c2abd34755
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-17 21:46:25 +0530

    update

--- 276C.cpp
+++ 276C.cpp
@@ -24,6 +24,37 @@
 #define test ll t; cin>>t; while(t--)
 #define ll long long
 ll v[999999];
-#define loop(i,l,h) for(ll i=(l);i<(h);i++)
+#define loop(i,l,h) for(ll i=(l);i<=(h);i++)
 using namespace std;
+ll aux[200005];
+ll ans[200005];
+ll arr[200005];
 int main(){
+    ll n,q;
+    cin>>n>>q;
+    ll a,b;
+    loop(i,1,n){
+        cin>>arr[i];
+    }
+    sort(arr+1,arr+n+1);
+    memset(aux,0,sizeof(aux));
+    while(q--){
+      cin>>a>>b;
+      aux[a]++;
+      if(b<n){
+        aux[b+1]--;
+      }
+    }
+   
+    ll sum = 0;
+    loop(i,1,n){
+       sum+=aux[i];
+       ans[i]=sum;
+    }
+    sort(ans+1,ans+n+1);
+    ll sol = 0;
+    loop(i,1,n){
+        sol+=(arr[i]*ans[i]);
+    }
+    cout<<sol<<endl;
+}
