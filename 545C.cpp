65ee337f180ed6d878656c53aa9045306b39bf89
kartik-raj7 <kartikeyrajgupta007@gmail.com>
2022-08-24 18:21:57 +0530

    545C

--- 545C.cpp
+++ 545C.cpp
@@ -27,7 +27,26 @@ ll v[999999];
 #define loop(i,l,h) for(ll i=(l);i<(h);i++)
 using namespace std;
 int main(){
-    test{
-
+    //  vector<vector<int,int>>v;
+    int n;
+    cin>>n;
+    pair<int,int>p[n];
+    ll counter = 0;
+      for(int i=0;i<n;i++){
+       int a,b;
+       cin>>a>>b;
+       p[i] = {a,b};
+      }
+      if(n==1)counter++;
+      else counter+=2;
+      loop(i,1,n-1){
+        if(p[i].first-p[i-1].first>p[i].second){
+            counter++;
+        }
+        else if(p[i].second<p[i+1].first-p[i].first){
+            counter++;
+            p[i].first+=p[i].second;
+        }
+      }
+      cout<<counter<<endl;
     }
-}
